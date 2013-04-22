8841d7510d5f4c031157be5d92f2cbe95b0bd1d6
John Lato <jwlato@gmail.com>
2013-04-22 23:12:49 +0800

    WIP add filledLinePlot

--- Scope/Plots/TwoD/Line.hs
+++ Scope/Plots/TwoD/Line.hs
@@ -18,11 +18,18 @@
 module Scope.Plots.TwoD.Line (
   linePlot
 , linePlotD
+
+, filledLinePlot
+, filledLinePlotD
+
+, minMeanMaxPlot
 ) where
 
 import Scope.Types
 import Diagrams.Prelude
+
 import qualified Data.Vector.Unboxed as V
+import Control.Arrow
 
 -- | A line plot for any @Real@ type.
 linePlot :: (V.Unbox x, V.Unbox y, Real x, Real y, Renderable (Path R2) b)
@@ -34,3 +41,25 @@ linePlotD :: (Renderable (Path R2) b) => Plot Double Double (Diagram b R2)
 linePlotD = Plot plotter emptyPlotInfo
     where
         plotter = fromVertices . map p2 . V.toList
+
+filledLinePlot :: (V.Unbox x, V.Unbox y, Real x, Real y, Renderable (Path R2) b)
+         => Plot x (y,y) (Diagram b R2)
+filledLinePlot = mapPlot realToFrac (realToFrac *** realToFrac) filledLinePlotD
+
+filledLinePlotD :: (Renderable (Path R2) b) => Plot Double (Double,Double) (Diagram b R2)
+filledLinePlotD = Plot plotter emptyPlotInfo
+    where
+        plotter xs = let lo = map p2 . map (second fst) $ V.toList xs
+                         hi = map p2 . map (second snd) $ V.toList xs
+                     in pathLikeFromTrail $ close $ fromVertices (lo ++ reverse hi)
+
+minMeanMaxPlot
+    :: (V.Unbox x, V.Unbox y, Real x, Real y, Renderable (Path R2) b)
+         => Plot x (y,y,y) (Diagram b R2)
+minMeanMaxPlot = Plot plotter emptyPlotInfo
+    where
+        plot1 = makePlot $ mapPlot id (\(_,b,c) -> (b,c)) filledLinePlot
+        plot2 = makePlot $ mapPlot id (\(a,_,_) -> a) linePlot
+        -- probably need to strut these out.  Easier if we add them later,
+        -- after the data's been normalized.
+        plotter = atop <$> plot1 <*> plot2
